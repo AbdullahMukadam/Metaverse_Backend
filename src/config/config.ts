780989347135c2a465d4e42fdbfb8724da218bc4
AbdullahMukadam <abdullahmukadam21@gmail.com>
2025-06-28 12:16:29 -0700

    again bro

--- src/config/config.ts
+++ src/config/config.ts
@@ -12,6 +12,6 @@ interface Config {
 export const config: Config = {
     PORT: Number(process.env.PORT) || 6000,
     NodeEnv: process.env.MODE || "development",
-    Origin: process.env.MODE === "development" ? "http://localhost:3000" : "",
+    Origin: process.env.MODE === "development" ? "http://localhost:3000" : "https://2dverse.vercel.app",
     DEFAULT_SPACE_ID: process.env.DEFAULT_SPACE_ID!
 }
